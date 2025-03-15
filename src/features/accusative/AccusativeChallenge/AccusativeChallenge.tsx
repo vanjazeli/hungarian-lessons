18f637a87c87fb5fd9a64e1b876fd839e8f1e071
vanjazeli <vanjazeli@gmail.com>
2025-03-15 19:13:15 +0100

    Fix: Get rid of some linting errors

--- src/features/accusative/AccusativeChallenge/AccusativeChallenge.tsx
+++ src/features/accusative/AccusativeChallenge/AccusativeChallenge.tsx
@@ -16,7 +16,7 @@ import { accusativeChallengeQuestions } from "./AccusativeChallenge.consts";
 import { useRef, useState } from "react";
 
 export const AccusativeChallenge = () => {
-  const [api, setApi] = useState<CarouselApi | null>(null); // eslint-disable-line
+  const [_, setApi] = useState<CarouselApi | null>(null);
 
   const numberOfQuestions = 10;
   const { current: questions } = useRef(
@@ -32,29 +32,24 @@ export const AccusativeChallenge = () => {
         opts={{ watchDrag: false }}
       >
         <CarouselContent>
-          {questions.map(
-            (
-              { noun, answer, translation }, // eslint-disable-line
-              index,
-            ) => (
-              <CarouselItem
-                className="flex aspect-square items-center justify-center"
-                key={index}
-              >
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Typography
-                      className="border-foreground border-b-2 border-dashed"
-                      variant="h2"
-                    >
-                      {noun}
-                    </Typography>
-                  </TooltipTrigger>
-                  <TooltipContent>{translation}</TooltipContent>
-                </Tooltip>
-              </CarouselItem>
-            ),
-          )}
+          {questions.map(({ noun, translation }, index) => (
+            <CarouselItem
+              className="flex aspect-square items-center justify-center"
+              key={index}
+            >
+              <Tooltip>
+                <TooltipTrigger>
+                  <Typography
+                    className="border-foreground border-b-2 border-dashed"
+                    variant="h2"
+                  >
+                    {noun}
+                  </Typography>
+                </TooltipTrigger>
+                <TooltipContent>{translation}</TooltipContent>
+              </Tooltip>
+            </CarouselItem>
+          ))}
         </CarouselContent>
       </Carousel>
       <Input />
