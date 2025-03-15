7eb388affcd37969e9f4a1ee879f3086843e79a9
vanjazeli <vanjazeli@gmail.com>
2025-03-15 19:11:50 +0100

    Fix: Get rid of some linting errors

--- src/features/accusative/AccusativeChallenge/AccusativeChallenge.tsx
+++ src/features/accusative/AccusativeChallenge/AccusativeChallenge.tsx
@@ -16,11 +16,9 @@ import { accusativeChallengeQuestions } from "./AccusativeChallenge.consts";
 import { useRef, useState } from "react";
 
 export const AccusativeChallenge = () => {
-  const [api, setApi] = useState<CarouselApi | null>(null);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [api, setApi] = useState<CarouselApi | null>(null); // eslint-disable-line
 
   const numberOfQuestions = 10;
-
   const { current: questions } = useRef(
     getRandomQuestions(accusativeChallengeQuestions, numberOfQuestions),
   );
@@ -34,24 +32,29 @@ export const AccusativeChallenge = () => {
         opts={{ watchDrag: false }}
       >
         <CarouselContent>
-          {questions.map(({ noun, answer, translation }, index) => (
-            <CarouselItem
-              className="flex aspect-square items-center justify-center"
-              key={index}
-            >
-              <Tooltip>
-                <TooltipTrigger>
-                  <Typography
-                    className="border-foreground border-b-2 border-dashed"
-                    variant="h2"
-                  >
-                    {noun}
-                  </Typography>
-                </TooltipTrigger>
-                <TooltipContent>{translation}</TooltipContent>
-              </Tooltip>
-            </CarouselItem>
-          ))}
+          {questions.map(
+            (
+              { noun, answer, translation }, // eslint-disable-line
+              index,
+            ) => (
+              <CarouselItem
+                className="flex aspect-square items-center justify-center"
+                key={index}
+              >
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Typography
+                      className="border-foreground border-b-2 border-dashed"
+                      variant="h2"
+                    >
+                      {noun}
+                    </Typography>
+                  </TooltipTrigger>
+                  <TooltipContent>{translation}</TooltipContent>
+                </Tooltip>
+              </CarouselItem>
+            ),
+          )}
         </CarouselContent>
       </Carousel>
       <Input />
