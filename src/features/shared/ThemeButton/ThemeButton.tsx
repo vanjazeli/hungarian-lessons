69f77b92611d64bd40f07e3a53c674f3aa9f2a31
vanjazeli <vanjazeli@gmail.com>
2025-03-17 17:24:23 +0100

    Fix: Revert theme changes

--- src/features/shared/ThemeButton/ThemeButton.tsx
+++ src/features/shared/ThemeButton/ThemeButton.tsx
@@ -7,11 +7,11 @@ export const ThemeButton = () => {
   const { t } = useTranslation("navigation");
 
   const [isDark, setIsDark] = useState(
-    document.documentElement.classList.contains("dark")
+    document.body.classList.contains("dark")
   );
 
   const handleClick = () => {
-    document.documentElement.classList.toggle("dark");
+    document.body.classList.toggle("dark");
     setIsDark((prev) => !prev);
   };
 
