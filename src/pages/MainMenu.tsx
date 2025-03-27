14fb438608c9461f921921d61d96a92c785f7478
vanjazeli <vanjazeli@gmail.com>
2025-03-27 20:45:34 +0100

    Fix: Remove unnecessary MainLayout from MainMenu component

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,29 +1,20 @@
-import {
-  Button,
-  Typography,
-  VerticalStack,
-  Badge,
-  MainLayout,
-} from "components";
+import { Button, Typography, VerticalStack, Badge } from "components";
 import { Link } from "react-router";
 
 export const MainMenu = () => {
   return (
-    <MainLayout>
-      <VerticalStack>
-        <Badge variant="outline">Main Menu</Badge>
-        <Typography variant="h1">
-          Let's improve your Hungarian skills together!
-        </Typography>
-        <Typography variant="p" muted>
-          Take part in interactive practice sessions designed to help you
-          improve your Hungarian skills through a variety of exercises and
-          challenges.
-        </Typography>
-        <Button asChild>
-          <Link to="/accusative">Accusative</Link>
-        </Button>
-      </VerticalStack>
-    </MainLayout>
+    <VerticalStack>
+      <Badge variant="outline">Main Menu</Badge>
+      <Typography variant="h1">
+        Let's improve your Hungarian skills together!
+      </Typography>
+      <Typography variant="p" muted>
+        Take part in interactive practice sessions designed to help you improve
+        your Hungarian skills through a variety of exercises and challenges.
+      </Typography>
+      <Button asChild>
+        <Link to="/accusative">Accusative</Link>
+      </Button>
+    </VerticalStack>
   );
 };
